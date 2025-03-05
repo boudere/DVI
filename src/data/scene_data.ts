92350229a6241a25efe549536ba0ebe734adebcf
CapiPrakan <jesalo03@ucm.es>
2025-03-05 12:32:44 +0100

    RESET

--- src/data/scene_data.ts
+++
@@ -1,9 +0,0 @@
-// nombre de las escenas
-export const SCENE_PATH = "/src/scenes/";
-
-export const SCENE_BOOTLOADER = "Bootloader";
-export const SCENE_PLAY = "ScenePlay";
-export const SCENE_MANAGER = "SceneManager";
-
-export const SCENE_PANTALLAS = "Pantallas";
-export const SCENE_DIALOGO = "Dialogo";
