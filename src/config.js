a17cb393fb2cf3e52bb60488f15d3bbf238e07fc
CarmenMS03 <carmenmiguelspinola@gmail.com>
2025-03-22 00:31:42 +0100

    Página web, fondo y logo

--- src/config.js
+++ src/config.js
@@ -9,13 +9,13 @@ import Phaser from "phaser";
 const config = {
     // Tamaño del canvas
     width: 1860,
-    height: 1358,
+    height: 1096,
 
     // width: 1860/1.2,
     // height: 1056+40,
 
     // Contenedor del canvas (donde queremos poner el juego en el html)
-    parent: 'contenedor',
+    parent: 'juego',
 
     // Físicas del juego
     physics: {
@@ -30,6 +30,11 @@ const config = {
         Pantallas,
         SceneManager
     ],
+
+    scale: {
+        mode: Phaser.Scale.EXACT_FIT,  // Ajusta el canvas al contenedor sin distorsión
+        autoCenter: Phaser.Scale.CENTER_BOTH  // Centra el juego en la pantalla
+    }
 }
 
 // Creamos el juego con la configuración
