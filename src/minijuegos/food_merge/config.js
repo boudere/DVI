177266d0e93d8ea572049e943c1863fbfd06fb55
boudere <sofia.boudere@gmail.com>
2025-04-07 17:20:48 +0200

    más minijuego

---
+++ src/minijuegos/food_merge/config.js
@@ -0,0 +1,4 @@
+const foodTypes = ['cerveza', 'cafe', 'magdalena', 'croassant', 'churros', 'donut', 'patatas', 'huevos', 'tortilla', 
+    'perrito', 'quesadilla', 'sandwich', 'croquetas', 'kebap', 'hamburguesa', 'menu'];
+
+    let score = 0;
