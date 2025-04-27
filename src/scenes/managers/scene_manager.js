4c61c1f50c4e78d404fbc7bdd6b521643569f396
CarmenMS03 <carmenmiguelspinola@gmail.com>
2025-04-27 20:02:27 +0200

    intentando linkear con el juego
    poquito a poquito suavesuavecito

--- src/scenes/managers/scene_manager.js
+++ src/scenes/managers/scene_manager.js
@@ -59,7 +59,7 @@ class SceneManager extends Managers {
          //this.add_scene(DIALOGO_MANAGER, DialogoManager);
  
          // minijuego manager:
-         //this.add_scene(MINIJUEGO_MANAGER, MinijuegosManager, 'JuegoOveja');
+         //this.add_scene(MINIJUEGO_MANAGER, MinijuegosManager, 'JuegoDiscoteca');
          this.add_scene(MINIJUEGO_MANAGER, MinijuegosManager);
 
         this.add_scene(AFINIDAD_DIALOGO_MANAGER, AfinidadDialogoManager);
@@ -90,13 +90,17 @@ class SceneManager extends Managers {
         }
     }
 
-    // se ejecuta cuando hay un cambio de manaegr
     signal_click(on_click) {
         this.scenes[this.currentScene].exit();
         this.currentScene = this._get_next_scene(on_click.scene);
-
-        this.scenes[this.currentScene].enter(on_click.name);
-
+    
+        if (on_click.scene === 'minijuego') {
+            // Si es minijuego, pasar TODO el on_click
+            this.scenes[this.currentScene].enter(on_click);
+        } else {
+            // Si es cualquier otra cosa, pasar solo el name
+            this.scenes[this.currentScene].enter(on_click.name);
+        }
     }
 
     // se ejecuta cuando se termina de crear una escena
